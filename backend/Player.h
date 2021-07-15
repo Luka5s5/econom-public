b6706228daea7e8e192989f4e8d2bde284f283c1
Nikita2305 <nikitanikitin2305@gmail.com>
2021-07-15 21:48:05 +0300

    Первый компиль

--- backend/Player.h
+++ backend/Player.h
@@ -1,33 +1,34 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 #include <set>
 #include <string>
 
-void print(std::string name, std::vector<double> v) {
+/*void p(std::string name, std::vector<double> v) {
     std::cout << name << ": ";
     for (auto t: v) 
         std::cout << t << " ";
     std::cout << std::endl;
 }
 
-void print(std::string name, std::vector<int> v) {
+void p(std::string name, std::vector<int> v) {
     std::cout << name << ": ";
     for (auto t: v) 
         std::cout << t << " ";
     std::cout << std::endl;
-}
+}*/
 
 struct Player {
 
     void print() {
         std::cout << id << std::endl;
-        print("curr", currencies);
-        print("res", resources);
-        print("builds", building_levels);
+        //p("curr", currencies);
+        //p("res", resources);
+        //p("builds", building_levels);
         // print("treaties0", treaties[0]);
         // print("treaties1", treaties[1]);
-        print("army", army);
+        //p("army", army);
         std::cout << "last_attack: " << last_attack << std::endl;
         std::cout << "ban: " << ban << std::endl;
         std::cout << "strategy: " << strategy << std::endl;
@@ -58,23 +59,23 @@ struct Player {
     int ban;
     int strategy;
     void mine_resources() {
-        resources[0] += building_level[0] * 4;
-        resources[1] += building_level[1] * 8;
-        resources[2] += building_level[2] * 4;
-        resources[3] += building_level[3] * 8;
-        resources[4] += building_level[4] * 6;
-        resources[5] += building_level[5] * 4;
-        resources[6] += building_level[6] * building_level[6];
+        resources[0] += building_levels[0] * 4;
+        resources[1] += building_levels[1] * 8;
+        resources[2] += building_levels[2] * 4;
+        resources[3] += building_levels[3] * 8;
+        resources[4] += building_levels[4] * 6;
+        resources[5] += building_levels[5] * 4;
+        resources[6] += building_levels[6] * building_levels[6];
         for (int i = 0; i <= 2; i++) {
-            resources[i] += building_level[7] * (building_level[i] - 1);
+            resources[i] += building_levels[7] * (building_levels[i] - 1);
         }
         for (int i = 3; i <= 5; i++) {
-            resources[i] += building_level[8] * (building_level[i] - 1);
+            resources[i] += building_levels[8] * (building_levels[i] - 1);
         }
         for (int i = 0; i < 7; i++) {
-            resources[i] += building_level[9] * 2;
+            resources[i] += building_levels[9] * 2;
         }
-        resources[6] += building_level[8] * (building_level[i] + 1); 
+        resources[6] += building_levels[8] * (building_levels[6] + 1); 
     }
     bool check_available_currencies(std::vector<double> required) {
         return less_or_equal(required, currencies);
